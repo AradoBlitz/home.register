985d32bdc4fa20218e002970edcca26b0a77a676
vilidmi <vilidmi@vilidmi>
2016-09-06 00:01:50 +0300

    Added failure logging to IndexPageTest.

--- src/test/java/home/register/webui/IndexPageTest.java
+++ src/test/java/home/register/webui/IndexPageTest.java
@@ -2,7 +2,16 @@ package home.register.webui;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.lang.annotation.Annotation;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,9 +19,39 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class IndexPageTest {
 	
+	private WebDriver driver = new HtmlUnitDriver();
+	
+	
+	@Rule
+	public final TestRule failurHandler = new TestRule() {
+		
+		@Override
+		public Statement apply(final Statement base,final Description arg1) {
+			// TODO Auto-generated method stub
+			return new Statement() {
+				
+				@Override
+				public void evaluate() throws Throwable {
+					try{
+						base.evaluate();
+					}catch (Throwable e) {
+						BufferedOutputStream buffer = new BufferedOutputStream(new FileOutputStream(new File("./" + arg1.getMethodName())));
+						try{
+							buffer.write(driver.getPageSource().getBytes());
+						} finally {
+							buffer.close();
+						}
+						throw e;
+					}
+					
+				}
+			};
+		}
+	};
+	
 	@Test
 	public void addNewMonthlyPaymentRecord() throws Exception {
-		WebDriver driver = new HtmlUnitDriver();
+		
 		driver.get("http://localhost:8080/home.register/");
 		driver.findElement(By.id("rent")).sendKeys("25000");
 		driver.findElement(By.id("date")).sendKeys("20-Aug-1985");
@@ -22,7 +61,7 @@ public class IndexPageTest {
 		driver.findElement(By.id("toPay")).sendKeys("3700");
 		driver.findElement(By.id("SaveMonthlyPayment")).submit();
 		
-				
+		driver.findElement(By.id("monthlyPaymentList"));	
 		assertEquals(driver.getPageSource()
 				,"HTTP method POST is not supported by this URL"
 				,driver.findElement(By.tagName("body")).getText());
