96384a4dd2c0db7daf799f1bc317ab1ecb3e197e
vilidmi <vilidmi@vilidmi>
2016-10-16 14:45:11 +0300

    Replaced method addPaymentRecord with uploadFile as service addMonthlyPayment.

--- src/main/java/home/register/rest/MonthlyPaymentService.java
+++ src/main/java/home/register/rest/MonthlyPaymentService.java
@@ -1,9 +1,14 @@
 package home.register.rest;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Map;
 
 import javax.enterprise.context.RequestScoped;
 import javax.imageio.ImageIO;
@@ -17,7 +22,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.io.IOUtils;
 import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
 import home.register.model.MonthlyPayment;
 import home.register.rest.data.MonthlyPaymentRepository;
@@ -40,6 +48,39 @@ public class MonthlyPaymentService {
 	@POST
 	@Path("addMonthlyPayment")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(MultipartFormDataInput input) {
+		Map<String,List<InputPart>> uploadForm = input.getFormDataMap();
+		try {
+			MonthlyPayment newPaymentRecord = new MonthlyPayment();
+			System.out.println(uploadForm.get("rent").get(0).getBodyAsString());
+			newPaymentRecord.rent = Integer.parseInt(uploadForm.get("rent").get(0).getBodyAsString());
+			newPaymentRecord.date = uploadForm.get("Date").get(0).getBodyAsString();
+			ByteArrayOutputStream buff = new ByteArrayOutputStream();
+			for(InputPart inputPart : uploadForm.get("Photo")){
+				InputStream inputStream = inputPart.getBody(InputStream.class, null);
+				buff.write(IOUtils.toByteArray(inputStream));
+			}			
+			BufferedImage image = ImageIO.read(new ByteArrayInputStream(buff.toByteArray()));
+			newPaymentRecord.imageUri="service/image/1";
+			monthlyPaymentRepository.addImage(image);
+		
+			newPaymentRecord.t1 = Double.parseDouble(uploadForm.get("T1").get(0).getBodyAsString());
+			newPaymentRecord.t2 = Double.parseDouble(uploadForm.get("T2").get(0).getBodyAsString());
+			newPaymentRecord.toPay =Integer.parseInt(uploadForm.get("To pay").get(0).getBodyAsString());
+			
+			monthlyPaymentRepository.addPaymentRecord(newPaymentRecord);
+			
+			System.out.println(image);
+			System.out.println(newPaymentRecord);
+			return Response.ok().build();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return Response.serverError().build();
+		} 
+		
+	}
+	
 	public void addPaymentRecord(@FormDataParam("rent") Integer rent,
 			@FormDataParam("Date") String date, 
 			@FormDataParam("image") InputStream imageSource,
